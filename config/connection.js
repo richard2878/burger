b72d26568b726f836c83ce1f051d3b87481043ec
Richard Antolin <richardantolin@gmail.com>
2020-09-17 16:04:16 -0700

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -5,17 +5,17 @@ if(process.env.JAWSDB_URL)
 {   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: "w1h4cr5sb73o944p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     
         // Your port; if not 3306
         port: 3306,
     
         // Your username
-        user: "root",
+        user: "um0h5r6l6e0doqjr",
     
         // Your password
-        password: "14Robins@1",
-        database: "burgers_db",
+        password: "a27k09i6nh1c40cj",
+        database: "pgyzgqohhohvq3vb",
     })
 }
 
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const routes = require("./controllers/burger_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
