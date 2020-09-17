cfd022f0592fe9bcd65b27363311f76c62159e2f
Richard Antolin <richardantolin@gmail.com>
2020-09-17 09:20:00 -0700

    fixed connection JawsDB

--- config/connection.js
+++ config/connection.js
@@ -21,7 +21,7 @@ if(process.env.JAWSDB_URL)
 
 connection.connect(function(err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
+      console.error("error connect: " + err.stack);
       return;
     }
     console.log("connected as id " + connection.threadId);
